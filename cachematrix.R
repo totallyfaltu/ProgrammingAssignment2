a8bff2bad045cfda65b993150c7d3e47000e3990
Kent <kent@totallyfaltu.com>
2014-05-23 19:04:40 -0700

    Implemented skeleton code
    The code was stubbed and this now implements that code for
    caching the inversion of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file is the functions needed for inverting
+## a matrix and caching that inversion as it is a
+## costly operation
 
-## Write a short comment describing this function
 
+## makeCacheMatrix - Storage function for caching the
+## results of the matrix inversion done by the
+## cacheSolve function
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(matrix_val) m <<- matrix_val
+    getinverse <- function() m
+    list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Computes the inverse of a passed matrix or the cached
+## value if the matrix has been calculated already
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
